d871b0c29f5972eaad8a341466fc3b118a766b23
Alex Williams <92amwilliams@gmail.com>
2025-09-02 13:46:44 +0000

    updated to use most new intermediate layer

--- sources/warehouse/tn_runs.sql
+++ sources/warehouse/tn_runs.sql
@@ -1,5 +1,3 @@
 select
     *
-from staging.stg_runs r
-left join staging.stg_run_locations srl on r.leg_id = srl.leg_id and r.source_database = srl.source_database
-where r.source_database = 'tn'
+from intermediate.int_mem_runs
